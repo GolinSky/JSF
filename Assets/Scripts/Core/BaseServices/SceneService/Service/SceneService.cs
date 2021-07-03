b26e6be65d25c807532148b2faf9e43d7eba85ed
GolinSky <golinsky97@gmail.com>
2021-07-03 19:29:49 +0300

    [MOD] Rebuild scene scenario logic

--- Assets/Scripts/Core/BaseServices/SceneService/Service/SceneService.cs
+++ Assets/Scripts/Core/BaseServices/SceneService/Service/SceneService.cs
@@ -2,9 +2,6 @@
 using Core.Utils.Scene;
 using EventHandlerUtils;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using Application = UnityEngine.Application;
-using SceneManager = UnityEngine.SceneManagement.SceneManager;
 
 namespace Core.BaseServices.SceneService.Service
 {
@@ -12,18 +9,20 @@ namespace Core.BaseServices.SceneService.Service
     {
         public const SceneType EntryScene = SceneType.Loading;
         private static LevelModel LevelModel;
+        private static SceneType PreviousScene = SceneType.Loading;
+
         public static Handler<SceneType> SceneLoadedHandler { get; } = new Handler<SceneType>();
         public static Handler<SceneType> SceneUnloadedHandler { get; } = new Handler<SceneType>();
         public static SceneType TargetLevel { get; private set; }
 
-        
+        public static SceneType CurrentSceneType => LevelModel.GetTypeByName(SceneTools.ActiveScene.path);
+
 
         [RuntimeInitializeOnLoadMethod]
         public static void Init()
         {
-            return;//todo: create scene model 
             LevelModel = ModelService.Service.ModelService.GetModel<LevelModel>();
-            SceneManager.sceneUnloaded += HandleUnloadedScene;
+
             if (Application.isEditor)
             {
                 var currentSceneType = LevelModel.GetTypeByName(SceneTools.ActiveScene.path);
@@ -42,28 +41,11 @@ namespace Core.BaseServices.SceneService.Service
             }
         }
 
-        private static void HandleUnloadedScene(Scene scene)
-        {
-            SceneType sceneType = LevelModel.GetTypeByName(scene.path);
-            SceneUnloadedHandler?.Invoke(sceneType);
-        }
-
-        #region LoadApi
-
-        public static void LoadEntry()
-        {
-            SceneTools.LoadScene(LevelModel.GetByType(EntryScene));
-        }
-
-        public static void Restart()
-        {
-            TargetLevel = LevelModel.GetTypeByName(SceneTools.ActiveScene.path);
-            SceneTools.LoadScene(LevelModel.GetByType(SceneType.Loading));
-        }
-
         public static void LoadScene(SceneType sceneType)
         {
+            PreviousScene = CurrentSceneType;
             TargetLevel = sceneType;
+            SceneUnloadedHandler?.Invoke(PreviousScene);
             SceneLoadedHandler?.Invoke(sceneType);
             SceneTools.LoadScene(LevelModel.GetByType(SceneType.Loading));
         }
@@ -73,12 +55,12 @@ namespace Core.BaseServices.SceneService.Service
             return SceneTools.LoadSceneAsync(LevelModel.GetByType(TargetLevel));
         }
 
-        #endregion
     }
 
     public enum SceneType
     {
         Unset = -1,
         Loading = 0,
+        Test = 1,
     }
 }
