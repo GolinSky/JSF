7b1ba1c2080f98c3f6f8ab3f5f8423ce980e1e20
GolinSky <golinsky97@gmail.com>
2021-07-08 10:01:08 +0300

    [MOD] Refactor example controller

--- Assets/Scripts/FrameworkCore/Examples/ExampleEntity/Controller/ExampleController.cs
+++ Assets/Scripts/FrameworkCore/Examples/ExampleEntity/Controller/ExampleController.cs
@@ -1,36 +1,41 @@
-﻿using System;
-using FrameworkCore.BaseServices.SceneService.Service;
+﻿using FrameworkCore.BaseServices.SceneService.Service;
 using FrameworkCore.Examples.ExampleEntity.Service;
 using FrameworkCore.Examples.ExampleEntity.View;
 using FrameworkCore.Patterns.MVC.Controller;
 using FrameworkCore.Patterns.MVC.Factory;
 using UnityEngine;
+using Zenject;
 
 namespace FrameworkCore.Examples.ExampleEntity.Controller
 {
-    public class ExampleController : Controller<ExampleView>
+    public class ExampleController : Controller<ExampleView, ExampleServiceLayer>
     {
-        private const string ExampleMessage = "Hello World";
+        private const string ExampleMessage = "Hello World ";
         private readonly ILevelService levelService;
+        private string context;
+
+        [Inject]
         public ExampleController(ExampleView view, ILevelService levelService, IServiceFactory serviceFactory) : base(view, serviceFactory)
         {
-            view.SetContext(ExampleMessage + DateTime.Now.Second);
             this.levelService = levelService;
         }
-        
+
         public override void Execute()
         {
             base.Execute();
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                Service.UpdateDto(true);
-                var context = Service.GetContext();
-                Debug.Log(context);
+                serviceLayer.UpdateDto(true);
+
+                Debug.Log(ExampleMessage+context);
                 levelService.LoadScene(SceneType.Example);
                 
             }
         }
 
-        private  ExampleServiceLayer Service => serviceFactory.GetService<ExampleServiceLayer>();
+        protected override void HandleServiceLayer()
+        {
+            context = serviceLayer.GetContext();
+        }
     }
 }
