a7d6f36f1d446c13ed01a1a0120adb877276b24b
GolinSky <golinsky97@gmail.com>
2021-11-05 19:32:29 +0200

    [ADD] Clever event

---
+++ Assets/MyPackage/Runtime/Scripts/BaseServices/EventEntity/CleverEvent.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace UnityEngine.MyPackage.Runtime.Scripts.BaseServices.EventEntity
+{
+    public class CleverEvent
+    {
+        private bool isInvoked;
+
+        private event Action Actions;
+
+        public void AddListener(Action listenerAction)
+        {
+            Actions += listenerAction;
+            if (!isInvoked || listenerAction == null)
+                return;
+            listenerAction();
+        }
+
+        public void RemoveListener(Action listenerAction) => Actions -= listenerAction;
+
+        public void Invoke()
+        {
+            if (Actions == null)
+                return;
+        
+            isInvoked = true;
+            Actions.Invoke();
+        }
+
+        public void Reset()
+        {
+            Actions = null;
+        }
+    }
+}
+
