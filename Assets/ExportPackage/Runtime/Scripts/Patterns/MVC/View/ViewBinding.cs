26077473c3dc1c9a71134f090add723dd19b39bd
GolinSky <golinsky97@gmail.com>
2023-04-26 18:39:56 +0300

    - add destroy method

--- Assets/ExportPackage/Runtime/Scripts/Patterns/MVC/View/ViewBinding.cs
+++ Assets/ExportPackage/Runtime/Scripts/Patterns/MVC/View/ViewBinding.cs
@@ -4,12 +4,20 @@ using Zenject;
 namespace CodeFramework.Runtime.View
 {
     public abstract class ViewBinding<TIViewController> : CustomMonoBehaviour
-    where TIViewController : IViewController
+        where TIViewController : IViewController
     {
-        [Inject]
-        protected TIViewController ViewController { get; private set; }
-        
+        [Inject] protected TIViewController ViewController { get; private set; }
 
+        public virtual void Destroy()
+        {
+            OnBeforeDestroy();
+            Destroy(gameObject);
+        }
 
-    }
+        protected virtual void OnBeforeDestroy()
+        {
+            
+        }
+
+}
 }
