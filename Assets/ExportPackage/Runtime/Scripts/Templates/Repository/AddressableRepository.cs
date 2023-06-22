a2b14a1ea420c5cdb32c9d672aa2ebde12a18298
GolinSky <golinsky97@gmail.com>
2023-06-22 15:59:45 +0300

    - fix load asset to sync

--- Assets/ExportPackage/Runtime/Scripts/Templates/Repository/AddressableRepository.cs
+++ Assets/ExportPackage/Runtime/Scripts/Templates/Repository/AddressableRepository.cs
@@ -1,24 +1,15 @@
-﻿using System.Threading.Tasks;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.AddressableAssets;
+
 
 namespace CodeFramework
 {
     public class AddressableRepository: IRepository<string>
     {
-        public TResource Load<TResource>(string key) where TResource : Object
+        public  TResource Load<TResource>(string key) where TResource : Object
         {
-            return LoadAsync<TResource>(key).Result;
-        }
-        
-        private async Task<TResource> LoadAsync<TResource>(string key) where TResource : Object
-        {
-            var op =Addressables.LoadAssetAsync<TResource>(key).Task;
-            var result = await op;
-            //return result;
-            return op.Result;
+            var asyncOperationHandle = Addressables.LoadAssetAsync<TResource>(key);
+            return asyncOperationHandle.WaitForCompletion();
         }
     }
-    
-  
 }
