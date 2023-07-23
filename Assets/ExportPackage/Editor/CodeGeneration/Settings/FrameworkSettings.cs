9d8d960f9f911915935c1062db13f5bd1b0c0ad8
Alex GolinSky <golinsky97@gmail.com>
2023-07-23 17:15:29 +0300

    - add view controller path folder changers

--- Assets/ExportPackage/Editor/CodeGeneration/Settings/FrameworkSettings.cs
+++ Assets/ExportPackage/Editor/CodeGeneration/Settings/FrameworkSettings.cs
@@ -6,10 +6,27 @@ namespace ExportPackage.Editor.CodeGeneration.Settings
 {
     public class FrameworkSettings
     {
-        [MenuItem("JSF/Settings/Change Root Folder", false, 1)]
+        [MenuItem("JSF/Settings/Change Folders Path/Root", false, 1)]
         private static void SelectRootFolder()
         {
-            new PathWindowHandler(new PathSelectionWindow("Root"), FrameworkPath.Root);
+            ChangeFolderPath(FrameworkPath.Root);
+        }
+        
+        [MenuItem("JSF/Settings/Change Folders Path/Controller", false, 1)]
+        private static void SelectControllerFolder()
+        {
+            ChangeFolderPath(FrameworkPath.Controller);
+        }
+        
+        [MenuItem("JSF/Settings/Change Folders Path/View", false, 1)]
+        private static void SelectViewFolder()
+        {
+            ChangeFolderPath(FrameworkPath.View);
+        }
+
+        private static void ChangeFolderPath(FrameworkPath path)
+        {
+            new PathWindowHandler(new PathSelectionWindow(path.ToString()), path);
         }
     }
 }
