d521ce97c46f30aea0c60cc72040b74b1523e665
Alex GolinSky <golinsky97@gmail.com>
2023-07-24 17:57:48 +0300

    - remove connection between editor and runtime scripts

--- Assets/ExportPackage/Runtime/Scripts/ConfigurationService/Configuration.cs
+++ Assets/ExportPackage/Runtime/Scripts/ConfigurationService/Configuration.cs
@@ -1,12 +1,11 @@
 ﻿using CodeFramework.Runtime.Controllers.BaseServices;
-using CodeFramework.Editor;
 
 namespace CodeFramework.Runtime.Controllers.ConfigurationService
 {
     public static class Configuration //can use smth which data can be changed  - create scriptable object and set default settings
     {
         public const string GameContextName = nameof(GameContext);
-        public const string ProjectSettingsName = nameof(ProjectSettings);
+        public const string ProjectSettingsName = "ProjectSettings";
 
 
         public static IRepository<string> GetDefaultRepository()
